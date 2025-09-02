ede35e2ef688928b3f34b08e42077bc955b3c3de
achraf <acherf2011@gmail.com>
2025-09-02 13:28:52 +0100

    api

--- app/api/[date]/route.js
+++ app/api/[date]/route.js
@@ -1,30 +1,25 @@
 import { NextResponse } from "next/server";
 
-const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type",
-};
-
 export async function GET(request, { params }) {
-  try {
-    const { date } = params;
-    
-    if(isNaN(date) && !isNaN(Date.parse(date))) {
-        const time = new Date(date);
-        return NextResponse.json({ unix:Math.floor(time.getTime()),utc:time.toUTCString() }, { status: 200 });
-      } else if(!isNaN(date)) {
-        const time = new Date(Number(date));
-        return NextResponse.json({ unix:Math.floor(time.getTime()),utc:time.toUTCString() }, { status: 200 });
-      }else{
-        console.log(Date.parse(date));
-        return NextResponse.json({ error:"Invalid date" }, { status: 404 });
-      }
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    return NextResponse.json(
-      { error: "Failed to fetch data" },
-      { status: 500 }
-    );
+  const { date } = params;
+
+  let time;
+
+  // if number
+  if (!isNaN(date)) {
+    time = new Date(Number(date));
   }
+  // if valid date string
+  else if (!isNaN(Date.parse(date))) {
+    time = new Date(date);
+  }
+  // invalid
+  else {
+    return NextResponse.json({ error: "Invalid Date" });
+  }
+
+  return NextResponse.json({
+    unix: time.getTime(),
+    utc: time.toUTCString(),
+  });
 }
--- app/api/route.js
+++ app/api/route.js
@@ -1,14 +1,9 @@
 import { NextResponse } from "next/server";
 
-const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type",
-};
-
 export async function GET() {
   const time = new Date();
-  const utc = time.toUTCString();
-  const unix = time.getTime();
-  return NextResponse.json({ unix, utc });
+  return NextResponse.json({
+    unix: time.getTime(),
+    utc: time.toUTCString(),
+  });
 }
