d6dfb692e405c97394da5351dfa64e93f0687904
achraf <acherf2011@gmail.com>
2025-09-03 06:46:57 +0100

    Fix entry point

--- app/api/shorturl/route.js
+++ app/api/shorturl/route.js
@@ -25,7 +25,7 @@ export async function POST(request) {
 
   if (!url) {
     return NextResponse.json(
-      { error: "Invalid URL" },
+      { error: "invalid url" },
       { status: 400, headers: { "Access-Control-Allow-Origin": "*" } }
     );
   }
@@ -52,7 +52,7 @@ export async function POST(request) {
     );
   } catch (error) {
     return NextResponse.json(
-      { error: "Invalid URL" ,errorDetails: error.message},
+      { error: "invalid url" ,errorDetails: error.message},
       { status: 400, headers: { "Access-Control-Allow-Origin": "*" } }
     );
   }
