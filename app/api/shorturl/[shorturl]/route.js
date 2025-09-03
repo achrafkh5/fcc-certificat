fb07d993bd972b4edcf2ef43aa4445c28ba30a09
achraf <acherf2011@gmail.com>
2025-09-03 02:59:09 +0100

    Fix entry point

---
+++ app/api/shorturl/[shorturl]/route.js
@@ -0,0 +1,41 @@
+import { NextResponse } from "next/server";
+import { MongoClient } from "mongodb";
+
+export const config = {
+  api: {
+    externalResolver: true,
+  },
+};
+
+let client;
+if (!client) {
+  client = new MongoClient(process.env.URI);
+  await client.connect();
+}
+const db = client.db("testdb");
+
+export async function GET(request, { params }) {
+  const shorturl = params?.shorturl; // from /api/shorturl/[shorturl]
+
+  try {
+    const urlEntry = await db.collection("urls").findOne({ short_url: parseInt(shorturl) });
+
+    if (urlEntry) {
+      const redirectUrl = urlEntry.original_url.startsWith("http")
+        ? urlEntry.original_url
+        : `http://${urlEntry.original_url}`;
+
+      return NextResponse.redirect(redirectUrl, 307);
+    } else {
+      return NextResponse.json(
+        { error: "No short URL found for the given input" },
+        { status: 404, headers: { "Access-Control-Allow-Origin": "*" } }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Server Error" },
+      { status: 500, headers: { "Access-Control-Allow-Origin": "*" } }
+    );
+  }
+}
---
+++ app/api/shorturl/route.js
@@ -0,0 +1,47 @@
+
+import { NextResponse } from "next/server";
+import { MongoClient } from "mongodb";
+export const config = {
+  api: {
+    externalResolver: true,
+  },
+};
+let client;
+if (!client) {
+  client = new MongoClient(process.env.URI);
+  await client.connect();
+}
+await client.connect();
+const db = client.db("testdb");
+
+export async function POST(request) {
+  const body = await request.text();
+  const params = new URLSearchParams(body);
+  const url = params.get("url");
+
+  try {
+    const validUrl = new URL(url);
+    const numbberOfUrls = await db.collection("urls").countDocuments();
+    const shortCode = numbberOfUrls+1; 
+    await db.collection("urls").insertOne({
+      original_url: validUrl.href,
+      short_url: shortCode,
+    });
+    return NextResponse.json(
+      { original_url: validUrl.href, short_url: shortCode },
+      {
+        status: 201,
+        headers: { "Access-Control-Allow-Origin": "*" },
+      }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Invalid URL" },
+      {
+        status: 400,
+        headers: { "Access-Control-Allow-Origin": "*" },
+      }
+    );
+  }
+}
+
